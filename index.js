23a5bdfaff684f8e2728c5a1804e0da19cea566d
l3xst1ng <howdy.martt@gmail.com>
2024-04-07 14:20:36 +0300

    Sending data with fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Add your code here
+
+// function submitData(userName, userEmail) {
+//   return fetch("http://localhost:3000/users", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify({
+//       userName,
+//       userEmail,
+//     }),
+//   })
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       document.body.innerHTML = object["id"];
+//     })
+//     .catch(function (error) {
+//       document.body.innerHTML = error.message;
+//     });
+// }
+
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document.body.innerHTML = object["id"];
+    })
+    .catch(function (error) {
+      document.body.innerHTML = error.message;
+    });
+}
